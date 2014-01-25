3e7c4c2d11d2437f746594d9b625d0ac635ed82a
JoeDailey <JosephLDailey@gmail.com>
2014-01-25 06:29:53 -0600

    standard form for get<->posts and API interaction

--- server.js
+++ server.js
@@ -32,10 +32,58 @@ app.listen(9001);
 app.get('/', function(req, res) {
     res.render('home', {});
 });
+//Wunderground//////////////////////////////////////////////////////////////////////////
+
+//Wunderground////////////////////////////////////////////////////////////////////////
+app.get('/wunderground', function(req, res) {
+	res.send([
+		"alerts",
+		"almanac",
+		"astronomy",
+		"conditions",
+		"currenthurricane",
+		"forecast",
+		"forecast10day",
+		"geolookup",
+		"history",
+		"hourly",
+		"hourly10day",
+		"planner",
+		"rawtide",
+		"satellite",
+		"tide",
+		"webcams",
+		"yesterday"]);
+});
+app.post('/wunderground/:func', function(req, res) { //cdde5330c637ed40
+    GET(wunderground+req.params.func+"/q/"+req.body.param+".json", res);
+});
+//ESPN//////////////////////////////////////////////////////////////////////////////////
 app.post('/espn/:function',function(req, res) {
 	switch (req.params.function) {
 		case 'now':
-			$.ajax(
-			res.send();
+			// $.ajax({
+
+			// });
+			res.send(200, {});
+			break;
+		}
 });
 
+
+var GET = function(url, res){
+	$.ajax({
+		url: url,
+		type: "GET",
+		dataType: "json",
+		error:function(error){
+			res.send(400, {'message':"Bad Request"});
+		},
+		success:function(result, status){
+			res.send(status, result);
+		}
+	});
+}
+
+//URL bases///////////////////////////////////////////////////////////
+var wunderground = "http://api.wunderground.com/api/cdde5330c637ed40/";
