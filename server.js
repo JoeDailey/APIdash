96875c4420c0954fe4a35045aa9727cef446c6fb
JoeDailey <JosephLDailey@gmail.com>
2014-01-25 16:21:39 -0600

    twilio almost working

--- server.js
+++ server.js
@@ -156,6 +156,35 @@ app.post('/sendgrid/send', function(req, res){
 	});
 });
 
+app.post('/twilio/sendmessage', function(req, res){
+	$.ajax({
+		url: "https://ACe088ee6d17bce77541ba206263955b8b:9e730cd406c655aea6a7e644be15c0fe@api.twilio.com/2010-04-01/Accounts.json",
+		type: "POST",
+		dataType: "json",
+		data:{From:"+16087290126"},
+		error:function(error){
+			res.send(400, {'message':error});
+		},
+		success:function(result, status){
+			if(req.body.to.indexOf('+')==-1)req.body.to = "+"+req.body.to;
+			$.ajax({
+				url: "https://api.twilio.com/2010-04-01/Accounts/ACe088ee6d17bce77541ba206263955b8b/Messages.json",
+				type: "POST",
+				dataType: "json",
+				data:{From:"+16087290126",
+					To:req.body.to,
+					Body:req.body.message
+				},
+				error:function(error){
+					res.send(400, {'message':error});
+				},
+				success:function(result, status){
+					res.send(status, result);
+				}
+			});
+		}
+	});
+});
 
 
 var GET = function(url, res){
