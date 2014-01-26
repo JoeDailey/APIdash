de7edaea840989e1a404af88fe0930ee672c7a84
Lucas Mullens <lucasmullens@gmail.com>
2014-01-26 04:42:22 -0800

    asdf

---
+++ src/modules/if_equal.js
@@ -0,0 +1,14 @@
+module.config({
+    'name': 'If A Equal To B',
+    'inputs': ['A', 'B'],
+    'outputs': 'A'
+});
+
+module.process(function() {
+    var out = null;
+    var a = Number(module.input("A"));
+    var b = Number(module.input("B"));
+    if (a == b || module.input("A") === module.input("B")) {
+        module.send("A", module.input("A"));
+    }
+});
