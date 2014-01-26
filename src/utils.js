848e1b3f8dfafcfc731d7709f89fbde4d69e84cc
Jon Morton <jonanin@gmail.com>
2014-01-25 20:56:15 -0800

    Foo

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,10 @@
 
-var ModuleUtils = {
+var ModuleUtils = function(module) {
+    this.module = module;
+};
 
+ModuleUtils.prototype.api = function(path, data, cb) {
+    xhr = $.post('/' + path, data);
+    xhr.done(cb);
+    this.module.addXHR(xhr);
 };
